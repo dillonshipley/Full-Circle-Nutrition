624415141cf05cf03f098ec641af3f855ff708c7
dshipley <dshipley@zagmail.gonzaga.edu>
2022-09-13 22:10:49 -0700

    Ultra basic layout w/ no refinement
    Kind of what I was thinking homepage would look like.... absolutely no refinement but I'll get back to it in a bit

--- macros/frontend/src/components/homepage/Body.js
+++ macros/frontend/src/components/homepage/Body.js
@@ -1,11 +1,13 @@
 function Body() {
     return ( 
         <div id = "mainBodyDiv">
-            hello
+            <div id = "homePageTitle">Goal to Table Nutrition </div>
             <div id = "homePageCardContainer">
-                <div id = "homePageCard1"></div>
-                <div id = "homePageCard1"></div>
-                <div id = "homePageCard1"></div>
+                <div className = "card" id = "homePageCard1">Build From the Ground Up</div>
+                <div className = "card" id = "homePageCard2">Use One of Our Tools</div>
+            </div>
+            <div class = "homepageLoginText">
+                Log in for the complete experience
             </div>
         </div>
     );
