c0fa03380b8780228cf135dbd4ca4743479216c8
zfoteff <zfoteff@zagmail.gonzaga.edu>
2022-11-24 17:28:06 -0800

    feat: Started creating ingredients crud operations

---
+++ ingredients/managers.py
@@ -0,0 +1,72 @@
+from uuid import UUID, uuid4
+
+from django.core.exceptions import ObjectDoesNotExist
+from django.db.models import Manager
+from django.db.utils import IntegrityError
+
+from .models import Ingredient
+
+
+class IngredientManager(Manager):
+    def create_ingredient(
+        self, 
+        name: str, 
+        vegetarian: bool,
+        calories: float,
+        fat: float,
+        protein: float,
+        units: str,
+    ) -> tuple[bool, (UUID | IntegrityError)]:
+        """Creates and validates new ingredient entries 
+        to the database. Returns the UUID of the new ingredient, or an 
+        error if the operation did not complete
+
+        Args:
+            name (str): _description_
+            vegetarian (bool): _description_
+            calories (float): _description_
+            fat (float): _description_
+            protein (float): _description_
+            units (str): _description_
+        Returns:
+            tuple: _description_
+        """
+        try:
+            self.create(
+                ingredient_id=uuid4(),
+                name=name,
+                vegetarian=vegetarian,
+                calories=calories,
+                fat=fat,
+                protein=protein,
+                units=units
+            ).clean()
+            return True, self.last().ingredient_id
+        except IntegrityError as e:
+            return False, e
+
+    def get_ingredient_by_id(self, ingredient_id: UUID) -> tuple[bool, (Ingredient | ObjectDoesNotExist)]:
+        """Retrieve an ingredient from the database using the ingredient_id as the query
+
+        Args:
+            ingredient_id (UUID): UUID of the ingredient model that should be restored
+        Returns:
+            tuple: _description_
+        """
+        try:
+            return True, self.get(ingredient_id=ingredient_id)
+        except ObjectDoesNotExist as e:
+            return False, e
+
+    def update_ingredient_by_id(self, updated_values: dict) -> bool:
+        pass
+
+    def delete_ingredients_by_id(self, ingredient_id: UUID) -> bool:
+        """Remove an ingedient from the db using the ingredient_id as a query
+
+        Args:
+            ingredient_id (UUID): _description_
+        Returns:
+            bool: _description_
+        """
+        pass
--- ingredients/models.py
+++ ingredients/models.py
@@ -1,11 +1,13 @@
-import uuid
+from uuid import UUID, uuid4
 
 from django.db import models
 from django.utils.timezone import now
 
+from .managers import IngredientManager
+
 
 class Ingredient(models.Model):
-    UNIT_CHOICES = (
+    __unit_choices = (
         ("CUP", "cups"),
         ("TBP", "table spoon"),
         ("TSP", "tea spoon"),
@@ -17,20 +19,45 @@ class Ingredient(models.Model):
         ("SLC", "slice"),
     )
 
-    ingredient_id = models.AutoField(
+    ingredient_id = models.UUIDField(
         name="ingredient_id",
         primary_key=True,
+        max_length=36,
+        unique=True,
         editable=False,
+        default=uuid4(),
     )
     name = models.CharField(name="name", max_length=80, null=False, unique=True)
     vegetarian = models.BinaryField(name="vegetarian", default=False)
-    calories = models.SmallIntegerField(name="calories")
-    fat = models.SmallIntegerField(name="fat")
-    protein = models.SmallIntegerField(name="protein")
+    calories = models.SmallIntegerField(
+        name="calories", null=False, default=0, editable=True
+    )
+    fat = models.SmallIntegerField(name="fat", null=False, default=0, editable=True)
+    protein = models.SmallIntegerField(
+        name="protein", null=False, default=0, editable=True
+    )
     units = models.CharField(
         name="units",
-        choices=UNIT_CHOICES,
+        choices=__unit_choices,
         max_length=3,
     )
-    create_date = models.DateTimeField(name="create_date", default=now)
+    create_date = models.DateTimeField(name="create_date", editable=False, default=now)
     modify_date = models.DateTimeField(name="modify_date", default=now)
+
+    objects = IngredientManager()
+
+    def serialize(self) -> dict:
+        return {
+            "ingredient_id": self.ingredient_id,
+            "name": self.name,
+            "vegetarian": self.vegetarian,
+            "calories": float(self.calories),
+            "fat": float(self.fat),
+            "protein": float(self.protein),
+            "units": self.get_units_display(),
+            "create_date": self.create_date,
+            "modify_date": self.modify_date,
+        }
+
+    def __str__(self) -> str:
+        return f"{self.name}({self.ingredient_id}) [Calories: {self.calories} Fat: {self.fat} Protein: {self.protein}]"
