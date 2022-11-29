eb47d70c3e2145111ef8d430a1143b4cdd961fb5
zfoteff <zfoteff@zagmail.gonzaga.edu>
2022-11-28 21:55:09 -0800

    feat: Create docker resources

--- macros_backend/settings.py
+++ macros_backend/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(env("DEBUG_MODE"))
 
-ALLOWED_HOSTS = ["0.0.0.0", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["*"]
 
 # Logging Config
 
